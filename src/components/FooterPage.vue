332798747fe27f080afd6f173e80cf65291b542b
Fleurent-Julien <julien.fleurent@edu.univ-fcomte.fr>
2023-05-10 09:50:32 +0200

    Mise en page du footer

---
+++ src/components/FooterPage.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <footer>
+    <div class="grid-cols-2">
+    <ul>
+        <li><a href="#">Mon compte</a></li>
+        <li><a href="#">Recettes</a></li>
+        <li><a href="#">Conseils d'experts</a></li>
+        <li><a href="#">Planning</a></li>
+    </ul>
+
+    <ul>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Besoin d'aide</a></li>
+        <li><a href="#">Mentions légales</a></li>
+    </ul>
+</div>
+
+<div class="legal">
+    <p>Projet réalisé dans le cadre d'un exercice pédagogique au département MMI de Montbéliard</p>
+    <p>Produit Vert 2023</p>
+</div>
+    </footer>
+</template>
