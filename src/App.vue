362d364d7e4a66dec32f807e35030918e001bdd0
Fleurent-Julien <julien.fleurent@edu.univ-fcomte.fr>
2023-05-18 18:00:58 +0200

    Index visible

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import Navigation from './components/Navigation.vue';
+import cardconseils from '@/components/cardconseils.vue';
+import Navigation from '@/components/Navigation.vue';
+import FooterPage from '@/components/FooterPage.vue';
 </script>
 
 <template>
+  <cardconseils></cardconseils>
+      <cardconseils></cardconseils>
+      <cardconseils></cardconseils>
+      <Navigation></Navigation>
+      <FooterPage></FooterPage>
   <RouterView />
 </template>
