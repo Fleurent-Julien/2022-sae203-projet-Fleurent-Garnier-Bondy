4654a0cd77344f99d25cb5f7fb05aef09158b964
Fleurent-Julien <julien.fleurent@edu.univ-fcomte.fr>
2023-05-18 13:06:05 +0200

    Création composant Card Conseils (Responsive)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
-import Inscription from "@/components/Cardrecettes.vue"
+import Inscription from "@/components/cardconseils.vue"
 import Navigation from './components/Navigation.vue';
 </script>
 
---
+++ src/components/cardconseils.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="border border-white rounded-xl mx-5 my-5 shadow-xl shadow-black/50">
+    <img src="../img/conseil.webp" alt="Conseil pour débutant" class="w-full"/>
+    <div class="m-5">
+        <div class="flex space-x-2 text-xs mt_5">
+          <p>10 février 2023</p> 
+          <p class="border-2 bg-gray-200 px-5 py-0.5 rounded-full text-gray-700 font-bold -mt-1">20 minutes</p>
+        </div>
+        <div class="space-y-0.5 mb-5">
+        <h2 class="font-bold text-lg">Conseils pratiques pour débutant</h2>
+        <p>Phillipe Etchebest</p>
+        <p class="text-sm">Paris - France</p>
+        </div>
+         </div>
+    </div>
+</template>
